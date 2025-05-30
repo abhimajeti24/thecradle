221dfa16baea0dadd9dc59428f9b4b1f9f67d9cd
Abhinav Majeti <abhimajeti24@gmail.com>
2025-05-30 22:23:01 +0530

    Created public-frontend folder along with Components folder with Navbar

--- admin-frontend/src/main.jsx
+++ admin-frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react';
+import './App.css'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
